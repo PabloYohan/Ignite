5ad16ed36b4b36902cb41c2c225ffa07f9fab86e
Pablo Yohan <pabloyohandias@gmail.com>
2023-11-26 14:39:19 -0300

    inicio do react

---
+++ Aulas/fundamentos-react/src/App.jsx
@@ -0,0 +1,10 @@
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <div>
+      <Post author = "Pablo Yohan" content = "Meu primeiro componente"/>
+      <Post author = "Diego Fernandes" content = "Um post qualquer" />
+    </div>
+  )
+}
