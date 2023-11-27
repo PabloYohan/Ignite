100550937807b0ba4b00955976bdc24d45bb77e2
Pablo Yohan <pabloyohandias@gmail.com>
2023-11-27 18:13:20 -0300

    Componetes da Header e Sidebar

--- Reactjs/Aulas/fundamentos-react/src/App.jsx
+++ Reactjs/Aulas/fundamentos-react/src/App.jsx
@@ -1,10 +1,20 @@
-import { Post } from "./Post"
+import { Post } from "./components/Post"
+import { Header } from './components/Header';
+import styles from './App.module.css'
+import './global.css'
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
-      <Post author = "Pablo Yohan" content = "Meu primeiro componente"/>
-      <Post author = "Diego Fernandes" content = "Um post qualquer" />
+      <Header/>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author = "Pablo Yohan" content = "Meu primeiro componente"/>
+          <Post author = "Diego Fernandes" content = "Um post qualquer" />
+        </main>
+      </div>
     </div>
   )
 }
