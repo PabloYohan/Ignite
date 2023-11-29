02b910b23c1b75b5f05c2abe5e7f6e2be341446b
Pablo Yohan <pabloyohandias@gmail.com>
2023-11-29 20:09:21 -0300

    Componete Post

--- Reactjs/Aulas/fundamentos-react/src/components/Post.jsx
+++ Reactjs/Aulas/fundamentos-react/src/components/Post.jsx
@@ -1,8 +1,27 @@
-export function Post(props){
-  return(
-    <div>
-      <strong>{props.author}</strong>
-      <p>{props.content}</p>
-    </div>
+import styles from './Post.module.css'
+
+export function Post(){
+  return (
+    <article className = {styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/PabloYohan.png" />
+          <div className={styles.authorInfo}>
+            <strong>Pablo Yohan</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="29 de Novembro às 19h40" dateTime="2023-11-29 19:39:40">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋 </p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p><a href="">👉 jane.design/doctorcare</a></p>
+
+        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+      </div>
+    </article>
   )
 }
