4c9961427905ace277175b900cb886f1b868c19d
Pablo Yohan <pablo.yohan@embraer.com.br>
2024-02-09 10:02:24 -0300

    Introdução a requisições e respostas http

--- NodeJS/Aulas/Fundamentos_Nodejs/src/server.js
+++ NodeJS/Aulas/Fundamentos_Nodejs/src/server.js
@@ -1,6 +1,26 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer( (req, res) => {
+  const {method, url} = req
+
+  if(method === 'GET' && url === '/users'){
+    return res
+    .setHeader('Content-type', 'aplication/json')
+    .end(JSON.stringify(users))
+  }
+
+  if(method === 'POST' && url === '/users'){
+    users.push({
+      id: 1,
+      name: 'Jonh Doe',
+      email: 'jonhdoe@example.com'
+    })
+
+    return res.end('Criação de usuario')
+  }
+
   return res.end("Hello Iginte");
 })
 
