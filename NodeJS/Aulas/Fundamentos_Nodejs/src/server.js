e13c6ee1e0d18007d0267216712bc9fd937ea986
Pablo Yohan <pablo.yohan@embraer.com.br>
2024-02-19 17:21:31 -0300

    Streams, buffers e middlewares

--- NodeJS/Aulas/Fundamentos_Nodejs/src/server.js
+++ NodeJS/Aulas/Fundamentos_Nodejs/src/server.js
@@ -1,27 +1,31 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 const users = []
 
-const server = http.createServer( (req, res) => {
+const server = http.createServer( async (req, res) => {
   const {method, url} = req
+
+  await json(req, res)
 
   if(method === 'GET' && url === '/users'){
     return res
-    .setHeader('Content-type', 'aplication/json')
-    .end(JSON.stringify(users))
+      .end(JSON.stringify(users))
   }
 
   if(method === 'POST' && url === '/users'){
+    const { name, email } = req.body
+
     users.push({
       id: 1,
-      name: 'Jonh Doe',
-      email: 'jonhdoe@example.com'
+      name,
+      email
     })
 
-    return res.end('Criação de usuario')
+    return res.writeHead(201).end()
   }
 
-  return res.end("Hello Iginte");
+  return res.writeHead(404).end();
 })
 
 server.listen(3333);
