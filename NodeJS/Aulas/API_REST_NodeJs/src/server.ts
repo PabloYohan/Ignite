6812e2e177f48cba2fabf885c309d33512abc38e
Pablo Yohan <pabloyohandias@gmail.com>
2024-02-24 14:00:43 -0300

    Configuração de Ambiente e do Banco de Dados

--- NodeJS/Aulas/API_REST_NodeJs/src/server.ts
+++ NodeJS/Aulas/API_REST_NodeJs/src/server.ts
@@ -1,17 +1,18 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const table = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions').select('*')
 
-  return table
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
