08a907379d462f367022bce536d2eb4f489c9dd2
Pablo Yohan <pabloyohandias@gmail.com>
2024-02-26 15:43:59 -0300

    Estruturação do desafio

---
+++ NodeJS/Desafios/DesafioNode02-DailyDiet/db/migrations/20240226170117_create-users.ts
@@ -0,0 +1,13 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.string('name').notNullable()
+    table.string('email').notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  knex.schema.dropTable('users')
+}
---
+++ NodeJS/Desafios/DesafioNode02-DailyDiet/src/database.ts
@@ -0,0 +1,15 @@
+import { env } from './env'
+import { Knex } from 'knex'
+import 'dotenv/config'
+
+export const config: Knex.Config = {
+  client: 'sqlite3',
+  connection: {
+    filename: env.DATABASE_URL,
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
