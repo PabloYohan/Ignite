40014cb75ed0a25861c65330ef0bd5894cba1e9c
Pablo Yohan <pabloyohandias@gmail.com>
2024-02-28 18:24:11 -0300

    Novo componente - Header

--- Reactjs/Aulas/03-dt-money/src/App.tsx
+++ Reactjs/Aulas/03-dt-money/src/App.tsx
@@ -1,13 +1,13 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Transactions } from './pages/tranactions'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle>
-        <h1>Hello World</h1>
-      </GlobalStyle>
+      <GlobalStyle />
+      <Transactions />
     </ThemeProvider>
   )
 }
