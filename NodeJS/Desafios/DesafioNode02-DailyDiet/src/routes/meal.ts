e5f6ca53081d6939b0bfc945ad789b425e821433
Pablo Yohan <pabloyohandias@gmail.com>
2024-03-04 15:51:45 -0300

    Criação das rotas de metricas e update

--- NodeJS/Desafios/DesafioNode02-DailyDiet/src/routes/meal.ts
+++ NodeJS/Desafios/DesafioNode02-DailyDiet/src/routes/meal.ts
@@ -11,6 +11,84 @@ export async function MealRoutes(app: FastifyInstance) {
     isOnDiet: z.boolean(),
   })
 
+  const idMealSchema = z.object({
+    id: z.string().uuid(),
+  })
+
+  app.put('/:id', async (request, reply) => {
+    const { userId } = request.cookies
+
+    const { id } = idMealSchema.parse(request.params)
+
+    const { description, isOnDiet, name } = mealSchema.parse(request.body)
+
+    await knex('meal')
+      .where({ id, userId })
+      .update({ mealDescription: description, isOnDiet, mealName: name })
+
+    return reply.status(204).send()
+  })
+
+  app.get(
+    '/summary',
+    {
+      preHandler: [checkUserLogged],
+    },
+    async (request) => {
+      const { userId } = request.cookies
+
+      const totalMeals = await knex('meal')
+        .where('userId', userId)
+        .count('id', { as: 'total' })
+        .first()
+
+      const totalOnDiet = await knex('meal')
+        .count('id', { as: 'total' })
+        .where({
+          userId,
+          isOnDiet: true,
+        })
+        .first()
+
+      const totalOffDiet = await knex('meal')
+        .count('id', { as: 'total' })
+        .where({
+          userId,
+          isOnDiet: false,
+        })
+        .first()
+
+      const meals = await knex('meal')
+        .select('*')
+        .where({ userId })
+        .orderBy('created_at')
+
+      const { bestStreakOnDiet } = meals.reduce(
+        (acc, meal) => {
+          if (meal.isOnDiet) {
+            acc.currentStreak += 1
+          } else {
+            acc.currentStreak = 0
+          }
+
+          if (acc.currentStreak > acc.bestStreakOnDiet) {
+            acc.bestStreakOnDiet = acc.currentStreak
+          }
+
+          return acc
+        },
+        { bestStreakOnDiet: 0, currentStreak: 0 },
+      )
+
+      return {
+        total: totalMeals?.total,
+        onDiet: totalOnDiet?.total,
+        offDiet: totalOffDiet?.total,
+        bestStreakOnDiet,
+      }
+    },
+  )
+
   app.post(
     '/',
     {
@@ -55,12 +133,9 @@ export async function MealRoutes(app: FastifyInstance) {
       preHandler: [checkUserLogged],
     },
     async (request) => {
-      const getMealSchema = z.object({
-        id: z.string().uuid(),
-      })
       const { userId } = request.cookies
 
-      const { id } = getMealSchema.parse(request.params)
+      const { id } = idMealSchema.parse(request.params)
 
       const meal = await knex('meal')
         .select('*')
@@ -82,17 +157,16 @@ export async function MealRoutes(app: FastifyInstance) {
       preHandler: [checkUserLogged],
     },
     async (request, reply) => {
-      const deleteMealSchema = z.object({
-        id: z.string().uuid(),
-      })
       const { userId } = request.cookies
 
-      const { id } = deleteMealSchema.parse(request.params)
+      const { id } = idMealSchema.parse(request.params)
 
       await knex('meal').delete().where({
         id,
         userId,
       })
+
+      return reply.status(204).send()
     },
   )
 }
